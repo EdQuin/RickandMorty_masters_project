fca84ef2f93615d1322505215b4c58171bb6a1be
edgar <edgar.quintero@ua.pt>
2022-12-08 12:16:01 +0000

    home intro aspect 50% done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Title } from "../styles";
+import { PrimeText, HomeImg } from "../styles";
+import Fundo from "../assets/fundo.jpg"
 
 
 export default function Home (){
 
     return (
-        <div>
-    <Title>This is Home</Title>
-    <h1>Here will be presented simple info</h1>
-    <h1>made for the pourpuse of testing Api Call</h1>
+        <div style={{display: "flex"}}>
+            <div style={{display: "inline"}}><HomeImg src={Fundo} alt="Rick and Morty"></HomeImg></div>
+            <div style={{display: "inline"}}><Title>Rick and Morty</Title>
+            <PrimeText>
+        Here will be presented simple info. Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. Pellentesque sollicitudin orci in 
+        diam posuere vestibulum. 
+    </PrimeText>
+    <PrimeText>made for the pourpuse of testing Api Call</PrimeText></div>
+    
+
+    
     </div>
     )
     
